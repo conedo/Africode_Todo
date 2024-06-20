5b225fd7891da5afb7c4c8cf968348a0a4bc84f9
Dolly Chepkorir <dollychepkorir@gmail.com>
2024-06-20 18:55:36 +0300

    new project

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://dolly:40053@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'{self.id} {self.description}'
+
+app.app_context().push()   
+db.create_all()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',todos=Todo.query.all())
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
